353d9902239d439d82ceeafab65278b15f51b8ed
gabrieldiaztoro <gdiaz_toro@hotmail.com>
2022-10-04 10:22:57 -0300

    adicionando box, hora, minuto, segundo, periodo

--- despertador.py
+++ despertador.py
@@ -51,11 +51,52 @@ l_imagem_despertador = Label(frame_baixo, text="ALARME",
                              height=1, anchor=NE, font=('Ivi 15 bold'), bg=cor1, fg=cor3)
 l_imagem_despertador.place(x=160, y=0)
 
-# criando box
-c_hora = Combobox(frame_baixo, width=2, font=('Ivi 15'))
+
+# criando box e labels dos box (HORAS)
+c_hora = Combobox(frame_baixo, width=2, font=('Ivi 12'))
 c_hora['value'] = ("00", "01", "02", "03", "04", "05",
                    "06", "07", "08", "09", "10", "11", "12")
 c_hora.current(0)
-c_hora.place(x=130, y=58)
+c_hora.place(x=130, y=50)
+
+l_imagem_horas = Label(frame_baixo, text="HORAS",
+                       height=1, anchor=NW, font=('Ivi 8 bold'), bg=cor1, fg=cor4)
+l_imagem_horas.place(x=130, y=30)
+
+
+# criando box e labels dos box (MINUTOS)
+c_minutos = Combobox(frame_baixo, width=2, font=('Ivi 12'))
+c_minutos['value'] = ("00", "01", "02", "03", "04", "05",
+                      "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59")
+c_minutos.current(0)
+c_minutos.place(x=180, y=50)
+
+l_imagem_minutos = Label(frame_baixo, text="MINUTOS",
+                         height=1, anchor=NW, font=('Ivi 8 bold'), bg=cor1, fg=cor4)
+l_imagem_minutos.place(x=175, y=30)
+
+
+# criando box e labels dos box (segundos)
+c_segundos = Combobox(frame_baixo, width=2, font=('Ivi 12'))
+c_segundos['value'] = ("00", "01", "02", "03", "04", "05",
+                       "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59")
+c_segundos.current(0)
+c_segundos.place(x=230, y=50)
+
+l_imagem_segundos = Label(frame_baixo, text="SEGUNDOS",
+                          height=1, anchor=NW, font=('Ivi 8 bold'), bg=cor1, fg=cor4)
+l_imagem_segundos.place(x=230, y=30)
+
+
+# criando box e labels dos box (PERIODO)
+c_periodo = Combobox(frame_baixo, width=3, font=('Ivi 12'))
+c_periodo['value'] = ("AM", "PM")
+c_periodo.current(0)
+c_periodo.place(x=290, y=50)
+
+l_imagem_periodo = Label(frame_baixo, text="PERIODO",
+                         height=1, anchor=NW, font=('Ivi 8 bold'), bg=cor1, fg=cor4)
+l_imagem_periodo.place(x=290, y=30)
+
 
 janela.mainloop()
